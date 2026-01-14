b820509636dd218bf157e6a2f01f315b04538dd9
Alpha OUMAR LENO <lenoalphaoumar@gmail.com>
2026-01-14 09:49:44 +0100

    refactor: move frontend to website_Clinique_rabat

--- react/src/composants/config/index.js
+++
@@ -1,5 +0,0 @@
-// Configuration des composants
-export { default as axiosInstance } from './axiosConfig';
-export { clearSession, isAuthenticated, getToken, getUserInfo } from './axiosConfig';
-export { default as authService } from './authService';
-export { API_BASE } from './apiconfig'; 
--- react/src/composants/shared/index.js
+++
@@ -1,3 +0,0 @@
-// Composants partagés
-export { default as Pagination } from './Pagination';
-export { default as UnifiedModal } from './UnifiedModal';
